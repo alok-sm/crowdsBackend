f8870ef699081be22594dfacf5227768ba57fc61
Mani <mani28shankar@gmail.com>
2015-03-12 16:28:10 +0530

    migration changes
    user and admin_user migration modified

---
+++ database/migrations/2015_03_12_104023_create_users.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsers extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->rememberToken();
+			$table->enum('experimental_condition', array('social', 'control'));
+			
+			$table->integer('age');
+			$table->enum('gender', array('M', 'F'));
+			$table->string('country');
+			
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
