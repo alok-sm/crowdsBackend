8c2a5a6226445ec0d5352dded50b6bd23f739e1f
Mani <mani28shankar@gmail.com>
2015-03-17 02:26:29 +0530

    changed table of confidence scores

---
+++ database/migrations/2015_03_16_204804_move_confidence_score_to_task_buffers.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MoveConfidenceScoreToTaskBuffers extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		//
+		Schema::table('answers', function($table)
+		{
+			$table->dropColumn('pre_confidence_value');
+			$table->dropColumn('post_confidence_value');
+		});
+		
+		Schema::table('task_buffers', function($table)
+		{
+			$table->string('pre_confidence_value');
+			$table->string('post_confidence_value');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+		
+		Schema::table('answers', function($table)
+		{
+			$table->string('pre_confidence_value');
+			$table->string('post_confidence_value');
+		});
+		
+
+		Schema::table('task_buffers', function($table)
+		{
+			$table->dropColumn('pre_confidence_value');
+			$table->dropColumn('post_confidence_value');
+		});
+
+		
+	}
+
+}
