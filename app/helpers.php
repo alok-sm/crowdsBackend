c2a014e84ac1778167510992ae32307bc2152a59
Mani <mani28shankar@gmail.com>
2015-03-18 16:39:31 +0530

    helper function added for TaskController

--- app/helpers.php
+++ app/helpers.php
@@ -1,65 +1,8 @@
-<?php namespace App\Http\Controllers;
+<?php
 
-use App\Http\Requests;
-use App\Http\Controllers\Controller;
-
-use Illuminate\Http\Request;
-
-class TaskController extends Controller {
-
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		//
-	}
-
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
-
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-		
-		
-	}
-
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-		
-	}
-	
-	public function assign()
-	
-	{
-	
-		$userId=\Request::input('userId');
-		
-		$response_array=helper($userId);
-		/*
-		if(isset($userId))
+function helper($userId)
+{
+  		if(isset($userId))
 		{
 			$result = \DB::table('task_buffers')->select('id','domain_id', 'task_id_list')->where('user_id', $userId)->orderBy('id','desc')->first();
 			$task_buffer_id=$result->id;
@@ -116,43 +59,6 @@ class TaskController extends Controller {
 		{
 			$response_array = array('status' => 'fail');
 		}
-		*/
-		return \Response::json($response_array, 200);
-	
+		return $response_array;
 	}
-	
-	
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
-
-}
+?>
