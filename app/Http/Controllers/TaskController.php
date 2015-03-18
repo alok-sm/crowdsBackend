f631a1c5aeb658b047a78da8f56c59fa245ec9d6
Mani <mani28shankar@gmail.com>
2015-03-18 16:18:54 +0530

    Working TaskController ...hopefully!

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -56,9 +56,12 @@ class TaskController extends Controller {
 	{
 	
 		$userId=\Request::input('userId');
+		
+		//$response_array=helper($userId);
+		
 		if(isset($userId))
 		{
-			$result = DB::table('task_buffer')->select('id','domain_id', 'task_id_list')->where('user_id', $userId)->last();
+			$result = \DB::table('task_buffers')->select('id','domain_id', 'task_id_list')->where('user_id', $userId)->orderBy('id','desc')->first();
 			$task_buffer_id=$result->id;
 			$domain_id=$result->domain_id;
 			$task=$result->task_id_list;
@@ -67,42 +70,42 @@ class TaskController extends Controller {
 			if($num_task>0){
 				$index=rand(0,$num_task-1);
 				$task_id=$task[$index];
-				$task_desc=DB::table('tasks')->select('id','title','type','data','answer_type','answer_data')->where('id', $task_id);
+				$task_desc=\DB::table('tasks')->select('id','title','type','data','answer_type','answer_data')->where('id', $task_id)->first();
 				$taskId=$task_desc->id;
 				$taskTitle=$task_desc->title;
 				$taskType=$task_desc->type;
 				$taskData=$task_desc->data;
 				$answerType=$task_desc->answer_type;
 				$answerData=$task_desc->answer_data;
-				$task_json=json(array("taskId"=>$taskId,"taskTitle"=>$taskTitle,"taskType"=>$taskType,"taskData"=>$taskData,"answerType"=>$answerType,"answerData"=>$answerData));
+				$task_json=(array("taskId"=>$taskId,"taskTitle"=>$taskTitle,"taskType"=>$taskType,"taskData"=>$taskData,"answerType"=>$answerType,"answerData"=>$answerData));
 				$response_array=array("status"=>"success","task"=>$task_json);
-				$task=array_splice($task,$index,1);
-				DB::table('task_buffer')->where('id', $task_buffer_id)->update(['task_id_list' => $task]);
+				//$task=array_splice($task,$index,1);
+				//DB::table('task_buffers')->where('id', $task_buffer_id)->update(['task_id_list' => $task]);
 			}
 			
 			else{
-				$domains = DB::table('task_buffer')->select('domain_id')->where('user_id', $userId)->get();
-				$domains_all=DB::table('domains')->select('id')->get();
+				$domains = \DB::table('task_buffers')->select('domain_id')->where('user_id', $userId)->get();
+				$domains_all=\DB::table('domains')->select('id')->get();
 				$diff=array_diff($domains_all->id,$domains->domain_id);
 				$size=sizeof($diff);
 				$index=rand(0,$size-1);
 				$domain_id=$diff[$index];
-				$task=DB::table('tasks')->select('task_id')->where('domain_id', $domain_id)->get();
+				$task=\DB::table('tasks')->select('task_id')->where('domain_id', $domain_id)->get();
 				$size=sizeof($task);
 				$index=rand(0,$size-1);
 				$task_id=$task[$index];
-				$task_desc=DB::table('tasks')->select('id','title','type','data','answer_type','answer_data')->where('id', $task_id);
+				$task_desc=\DB::table('tasks')->select('id','title','type','data','answer_type','answer_data')->where('id', $task_id);
 				$taskId=$task_desc->id;
 				$taskTitle=$task_desc->title;
 				$taskType=$task_desc->type;
 				$taskData=$task_desc->data;
 				$answerType=$task_desc->answer_type;
 				$answerData=$task_desc->answer_data;
-				$task_json=json(array("taskId"=>$taskId,"taskTitle"=>$taskTitle,"taskType"=>$taskType,"taskData"=>$taskData,"answerType"=>$answerType,"answerData"=>$answerData));
+				$task_json=(array("taskId"=>$taskId,"taskTitle"=>$taskTitle,"taskType"=>$taskType,"taskData"=>$taskData,"answerType"=>$answerType,"answerData"=>$answerData));
 				$response_array=array("status"=>"success","task"=>$task_json);
-				$task=array_splice($task,$index,1);
+				//$task=array_splice($task,$index,1);
 				
-				DB::table('task_buffers')->insert(['user_id' => $userId, 'domain_id' => $domain_id, 'task_id_list'=>$task]);
+				//DB::table('task_buffers')->insert(['user_id' => $userId, 'domain_id' => $domain_id, 'task_id_list'=>$task]);
 				
 			}
 			
@@ -115,6 +118,7 @@ class TaskController extends Controller {
 		}
 		
 		return \Response::json($response_array, 200);
+	
 	}
 	
 	
