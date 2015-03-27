51c620888711efa2a003737d26de2771bc971208
Vikas S Yaligar <vikasyaligar.it@gmail.com>
2015-03-27 20:53:35 +0530

    Removed the remove_column which was written twice

--- database/migrations/2015_03_16_204804_move_confidence_score_to_task_buffers.php
+++ database/migrations/2015_03_16_204804_move_confidence_score_to_task_buffers.php
@@ -52,8 +52,7 @@ class MoveConfidenceScoreToTaskBuffers extends Migration {
 		});
 		
 		Schema::table('task_buffers', function($table)
-		{
-			// $table->dropColumn('pre_confidence_value');
+		{ 	
 			$table->dropColumn('post_confidence_value');
 		});
 
