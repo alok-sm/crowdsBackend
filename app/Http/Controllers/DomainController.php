fe59e559dbbcdfcf209c153bde2f25cd50e72012
Vikas S Yaligar <vikasyaligar.it@gmail.com>
2015-03-27 13:57:49 +0530

    Added Domain Controller to avoid a bit of conflict

---
+++ app/Http/Controllers/DomainController.php
@@ -0,0 +1,78 @@
+<?php namespace App\Http\Controllers;
+
+use App\TaskBuffer;
+use Illuminate\Cookie\CookieJar;
+use App\Http\Controllers\Controller;
+
+class DomainController extends Controller {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		//
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		//
+	}
+
+	public function store()
+	{
+		
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+}
