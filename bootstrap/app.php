2b3c9bbde8fdb356f01025e533775998c9acc68e
Vikas S Yaligar <vikasyaligar.it@gmail.com>
2015-03-29 23:23:19 +0530

    Updated the new header

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -58,9 +58,10 @@ $app->singleton(
 if (!(getenv("DATABASE_URL") == NULL))
 {
 	// allow origin
-	header('Access-Control-Allow-Origin: *');
-	header('Access-Control-Allow-Methods: GET, POST');
-	header("Access-Control-Allow-Headers: X-Requested-With");
+	header('Access-Control-Allow-Origin: http://127.0.0.1:4567');
+	header('Access-Control-Allow-Credentials: true');
+	header('Access-Control-Allow-Methods: GET, POST,PUT,DELETE,PATCH,OPTIONS');
+	header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 }
 
 return $app;
