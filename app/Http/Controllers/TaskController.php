b41bd1097e29116fa0a1ba3f4a9f89fc4aed669d
Vikas S Yaligar <vikasyaligar.it@gmail.com>
2015-07-09 22:20:54 +0530

    Moved the /rank API call to task controller as domain controller seems to have a bug

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -52,21 +52,46 @@ class TaskController extends Controller {
 		
 	}
 	
-public function assign()
-{
-	
-	$token= \Request::input('token');
+	public function assign()
+	{
+		
+		$token= \Request::input('token');
 
-	$status = Client::where('token', '=', $token)->first();
+		$status = Client::where('token', '=', $token)->first();
 
-	if ($status != null)
-		$response_array = helper($status->id);
-	else
-		$response_array = array("status" => "fail");
+		if ($status != null)
+			$response_array = helper($status->id);
+		else
+			$response_array = array("status" => "fail");
 
-	return \Response::json($response_array, 200);
-}
-	
+		return \Response::json($response_array, 200);
+	}
+
+	public function rank()
+	{
+		$token= \Request::input('token');
+		$status = Client::where('token', '=', $token)->first();
+		$rank = null;
+		$total_users = null;
+
+		if ($status != null)
+		{
+			$user_id = $status->id;
+			$task_buffer = TaskBuffer::where('user_id', $user_id)->where('task_id_list', '[]')->orderBy('id','desc')->first();
+			if (isset($task_buffer))
+			{
+				$rank = TaskBuffer::where('domain_id', $task_buffer->domain_id)->where('task_id_list', '[]')->where('points', '<', $task_buffer->points)->count();
+				$total_users = TaskBuffer::where('domain_id', $task_buffer->domain_id)->where('task_id_list', '[]')->count())
+			}
+			$domain_done = Client::find($user_id)->task_buffers()->count();
+			$total_domains = Domain::all()->count();
+			$remaining_domains = $total_domains - $domain_done + 1;
+
+			return {"status" => "success", "remaining_domains" => $remaining_domains, "total_domains" => $total_domains, "rank" => $rank, "total_users" => $total_users, "points" => $task_buffer->points}
+		}
+		else
+			return {"status" => "fail"}
+	}
 	
 	/**
 	 * Show the form for editing the specified resource.
