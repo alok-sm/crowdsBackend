e8bdb275b9b6a4c99ddc3b64fc1389c6c8b13fe4
Vikas S Yaligar <vikasyaligar.it@gmail.com>
2015-07-09 22:33:46 +0530

    Returning json response

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -86,11 +86,13 @@ class TaskController extends Controller {
 			$domain_done = Client::find($user_id)->task_buffers()->count();
 			$total_domains = Domain::all()->count();
 			$remaining_domains = $total_domains - $domain_done + 1;
-
-			return {"status" => "success", "remaining_domains" => $remaining_domains, "total_domains" => $total_domains, "rank" => $rank, "total_users" => $total_users, "points" => $task_buffer->points}
+			$response_array = { "status" => "success", "remaining_domains" => $remaining_domains, "total_domains" => $total_domains, "rank" => $rank, "total_users" => $total_users, "points" => $task_buffer->points }
 		}
 		else
-			return {"status" => "fail"}
+		{
+			$response_array = { "status" => "fail" }
+		}
+		return \Response::json($response_array, 200);
 	}
 	
 	/**
