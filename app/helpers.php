09bcc7e82bbcb3d17bc8b46d9bbdad4e7464fc56
Vikas S Yaligar <vikasyaligar.it@gmail.com>
2015-10-27 20:42:12 +0530

    Patching up continous valued questions

--- app/helpers.php
+++ app/helpers.php
@@ -184,7 +184,8 @@ function select_domain($domain_id_list)
 function assign_random_domain($user_id)
 {
 	$user_domains = TaskBuffer::where('user_id', $user_id)->lists('domain_id');
-	$all_domains = Domain::lists('id');
+	// TODO: Making a hack to show only the absolute 
+	$all_domains = Domain::where('id', '!=', 19)->lists('id');
 	$diff = array_diff($all_domains, $user_domains);
 	$diff = array_values($diff);
 
