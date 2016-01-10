e7892dfff0b89377e561b447c2dd6e967206fd89
Vikas S Yaligar <vikasyaligar.it@gmail.com>
2016-01-10 18:28:20 +0530

    Added support for crowd ranking

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -1,5 +1,6 @@
 <?php namespace App\Http\Controllers;
 
+use DB;
 use App\Client;
 use App\TaskBuffer;
 use App\Domain;
@@ -91,18 +92,56 @@ class TaskController extends Controller {
 			$total_domains = Domain::all()->count();
 			$remaining_domains = $total_domains - $domain_done;
 			$points = 0;
-			if (strcmp($task_type, "int") == 0)
+			if (strcmp($task_type, "int") == 0) {
+				$crowd_points = calculate_int_points($task_buffer->domain_id);
+				$crowd_rank = TaskBuffer::where('domain_id', $task_buffer->domain_id)->where('task_id_list', '[]')->where('points', '<', $crowd_points)->count();
 				$points = Answer::where('user_id', $user_id)->whereIn('task_id', Task::where('domain_id', $task_buffer->domain_id)->lists('id'))->where('points', 0)->count();
-			else
+			}
+			else {
+				$crowd_points = calculate_mcq_points($task_buffer->domain_id);
+				$crowd_rank = TaskBuffer::where('domain_id', $task_buffer->domain_id)->where('task_id_list', '[]')->where('points', '>', $crowd_points)->count();
 				$points = $task_buffer->points;
+			}
 
-			$response_array = array("status" => "success", "remaining_domains" => $remaining_domains, "total_domains" => $total_domains, "rank" => $rank + 1, "total_users" => $total_users, "points" => $points, "completion_code" => $task_buffer->completion_code);
+			$response_array = array("status" => "success", "remaining_domains" => $remaining_domains, "total_domains" => $total_domains, "rank" => $rank + 1, "total_users" => $total_users, "points" => $points, "completion_code" => $task_buffer->completion_code, "crowd_rank" => $crowd_rank + 1);
 		}
 		else
 		{
 			$response_array = array("status" => "fail");
 		}
 		return \Response::json($response_array, 200);
+	}
+
+	private function calculate_mcq_points($domain_id) {
+		$task_object = DB::select('select a.task_id, a.data, count(a.data) as count, t.correct_answer from tasks t, answers a, domains d where d.id = t.domain_id and t.id = a.task_id and t.domain_id = ? and a.data != "null" and a.data != "timeout" group by a.task_id, a.data', [$domain_id]);
+		$points = 0;
+		$task_id = 0;
+		$max = 0; // This needs to be initialized
+		$correct_answer_points = -1;
+		foreach ($task_object as $object) {
+			if ($task_id != intval($object->task_id)) {
+				if ($correct_answer_points >= $max) {
+					$points += 1;
+					var_dump($task_id);
+				}
+				$max = 0;
+				$task_id = intval($object->task_id);
+			}
+			if (strcmp($object->data, $object->correct_answer) == 0)
+				$correct_answer_points = intval($object->count);
+			if (intval($object->count) > $max)
+				$max = intval($object->count);
+		}
+		return $points;
+	}
+
+	private function calculate_int_points($domain_id) {
+		$task_object = DB::select('select avg(a.data) as average, t.correct_answer as correct_answer, t.id as task_id from tasks t, answers a, domains d where d.id = t.domain_id and t.id = a.task_id and t.domain_id = ? and a.data != "null" and a.data != "timeout" group by a.task_id', [$domain_id]);
+		$points = 0;
+		foreach ($task_object as $object) {
+			$points += abs($object->correct_answer - $object->average)
+		}
+		return $points;
 	}
 	
 	/**
