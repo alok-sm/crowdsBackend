e10b8f10eccf4621da965a6f191a777fc4e88c16
Alok Mysore <alok.shankar.m@gmail.com>
2016-11-05 16:28:45 -0700

    add web to gitignore

--- public/web2/js/app.js
+++
@@ -1,3 +0,0 @@
-var app = angular.module('app', ['ngRoute', 'ngAnimate', 'ngStorage', 'easypiechart']);
-
-app.constant('SITE_NAME', 'Wisdom of Crowds');
--- public/web2/js/services/logger.js
+++
@@ -1,7 +0,0 @@
-app.factory('Logger', function() {
-    var logger = {};
-    logger.log = function(str) {
-        console.log(str);
-    };
-    return logger;
-});
