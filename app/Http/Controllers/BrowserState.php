7520053f766f10c1d51ad3bad2876152a34a844d
Alok Mysore <alok.shankar.m@gmail.com>
2016-11-05 15:29:47 -0700

    flesh out BrowserState

--- app/Http/Controllers/BrowserState.php
+++ app/Http/Controllers/BrowserState.php
@@ -1,5 +1,8 @@
 <?php namespace App\Http\Controllers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
@@ -8,10 +11,20 @@ use Illuminate\Http\Request;
 class BrowserState extends Controller {
 	public function store($key, $val)
 	{
-
+		$browser_state = DB::table('browser_state');
+		$browser_state::firstOrCreate(['key' => $key, 'val' => $val]);
+		return array('success' => TRUE);
 	}
+
 	public function show($key)
 	{
+		$browser_state = DB::table('browser_state')->select('val')->where("key", "=", $key)->get();
 
+		foreach($browser_state as $state)
+		{
+			return array('success' => TRUE, 'data' => $state->val);
+		}
+
+		return arrray('success' => FALSE);
 	}
 }
