7dfd3231ec4a5445a58050d06bbd912885f76925
Alok Mysore <alok.shankar.m@gmail.com>
2016-11-05 16:22:44 -0700

    fix UserRank

--- app/Http/Controllers/UserRank.php
+++ app/Http/Controllers/UserRank.php
@@ -1,16 +1,21 @@
 <?php namespace App\Http\Controllers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
 
 class UserRank extends Controller {
-	public function store($user_id, $domain_id, $rank)
+	public function store()
 	{
 		$user_id = \Request::input('user_id');
 		$domain_id = \Request::input('domain_id');
 		$rank = \Request::input('rank');
 		
+		DB::statement("INSERT INTO user_domain_rank VALUES ('$user_id','$domain_id','$rank')");
+
+		return array('success' => TRUE);
 	}
 }
